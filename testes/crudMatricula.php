05585ce29ae7f8778234bcbd87a79caefd3184f9
jocile <jocile@gmail.com>
2025-08-29 10:35:12 -0300

    Add CRUD matricula

---
+++ testes/crudMatricula.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>CRUD de Matrícula</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <style>
+        .wrapper{
+            width: 600px;
+            margin: 0 auto;
+        }
+        table tr td:last-child{
+            width: 120px;
+        }
+    </style>
+    <script>
+        $(document).ready(function(){
+            $('[data-toggle="tooltip"]').tooltip();   
+        });
+    </script>
+</head>
+<body>
+<!--
+Tutorial para criar este modelo de CRUD : 
+https://www.tutorialrepublic.com/php-tutorial/php-mysql-crud-application.php
+-->
+
+  <div class="wrapper">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="mt-5 mb-3 clearfix">
+                        <h2 class="pull-left">Detalhes das matrículas</h2>
+                        <a href="adicionarMatricula.php" class="btn btn-success pull-right"><i class="fa fa-plus"></i> Adicionar nova matrícula</a>
+                    </div>
+                    <?php
+                    // Include config file
+                    require_once "../backend/conexao.php";
+                    
+                    // Attempt select query execution
+                    $sql = "SELECT * FROM matricula";
+                    if($result = $conexao->query($sql)){
+                        if($result->num_rows > 0){
+                            echo '<table class="table table-bordered table-striped">';
+                                echo "<thead>";
+                                    echo "<tr>";
+                                        echo "<th>#</th>";
+                                        echo "<th>Nome</th>";
+                                        echo "<th>Curso</th>";
+                                    echo "</tr>";
+                                echo "</thead>";
+                                echo "<tbody>";
+                                while($row = $result->fetch_array()){
+                                    echo "<tr>";
+                                        echo "<td>" . $row['id'] . "</td>";
+                                        echo "<td>" . $row['nome'] . "</td>";
+                                        echo "<td>" . $row['curso'] . "</td>";
+                                        echo "<td>";
+                                            echo '<a href="lendoMatricula.php?id='. $row['id'] .'" class="mr-3" title="View Record" data-toggle="tooltip"><span class="fa fa-eye"></span></a>';
+                                            echo '<a href="atualizandoMatricula.php?id='. $row['id'] .'" class="mr-3" title="Update Record" data-toggle="tooltip"><span class="fa fa-pencil"></span></a>';
+                                            echo '<a href="deletandoMatricula.php?id='. $row['id'] .'" title="Delete Record" data-toggle="tooltip"><span class="fa fa-trash"></span></a>';
+                                        echo "</td>";
+                                    echo "</tr>";
+                                }
+                                echo "</tbody>";                            
+                            echo "</table>";
+                            // Free result set
+                            $result->free();
+                        } else{
+                            echo '<div class="alert alert-danger"><em>No records were found.</em></div>';
+                        }
+                    } else{
+                        echo "Oops! Something went wrong. Please try again later.";
+                    }
+                    
+                    // Close connection
+                    $conexao->close();
+                    ?>
+                </div>
+            </div>        
+        </div>
+    </div>    
+</body>
+</html>
